572a456e969d3e2544e07413f74e3a3dd644d6a8
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2023-10-29 14:53:36 -0700

    Returns

--- src/delegates/HarmonyDelegate.php
+++ src/delegates/HarmonyDelegate.php
@@ -4,8 +4,6 @@ namespace Hiraeth\Harmony;
 
 use Hiraeth;
 use WoohooLabs\Harmony\Harmony;
-use Psr\Http\Message\ServerRequestInterface as ServerRequest;
-use Psr\Http\Message\ResponseInterface as Response;
 
 /**
  *
@@ -34,22 +32,11 @@ class HarmonyDelegate implements Hiraeth\Delegate
 	 */
 	public function __invoke(Hiraeth\Application $app): object
 	{
-		$harmony    = new Harmony($app->get(ServerRequest::class), $app->get(Response::class));
-		$middleware = $app->getConfig('*', 'middleware', []);
+		$harmony = $app->get(Harmony::class);
+		$manager = $app->get(Hiraeth\Middleware\Manager::class);
 
-		usort($middleware, function($a, $b) {
-			$a_priority = $a['priority'] ?? 50;
-			$b_priority = $b['priority'] ?? 50;
-
-			return $a_priority - $b_priority;
-		});
-
-		foreach ($middleware as $config) {
-			if (!$config || $config['disabled'] ?? FALSE) {
-				continue;
-			}
-
-			$harmony->addMiddleware($app->get($config['class']));
+		foreach ($manager->getAll() as $middleware){
+			$harmony->addMiddleware($middleware);
 		}
 
 		return $harmony;
